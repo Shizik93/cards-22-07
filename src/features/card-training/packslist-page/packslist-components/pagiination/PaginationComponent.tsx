ed4b1392926abb34bcf4eea5db27243c2cc04283
DeniKonst <denikonst.pl@gmail.com>
2022-08-04 17:07:44 +0300

    Created cardsPackList and cardsList, comment PaginationComponent

--- src/features/card-training/packslist-page/packslist-components/pagiination/PaginationComponent.tsx
+++ src/features/card-training/packslist-page/packslist-components/pagiination/PaginationComponent.tsx
@@ -1,37 +1,37 @@
 import React from "react";
-import {useState} from "react";
-import {useSelector} from "react-redux";
-import {AppStoreType} from "../../../../../app/store";
-import {CardPackItemsType} from "../../api-packslist/api-packsList";
-import {Pagination, TablePagination} from "@mui/material";
+// import {useState} from "react";
+// import {useSelector} from "react-redux";
+// import {AppStoreType} from "../../../../../app/store";
+// import {CardPackItemsType} from "../../api-packslist/api-packsList";
+// import {Pagination, TablePagination} from "@mui/material";
 
 
 const PaginationComponent = () => {
-    const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(5);
-    let selector = useSelector<AppStoreType, Array<CardPackItemsType>>(state => state.packsList.cardPacks)
-    const handleChangePage = (event: unknown, newPage: number) => {
-        setPage(newPage);
-    };
-    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
-    };
-    const emptyRows =
-        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - selector.length) : 0;
+    // const [page, setPage] = useState(0);
+    // const [rowsPerPage, setRowsPerPage] = useState(5);
+    // let selector = useSelector<AppStoreType, Array<CardPackItemsType>>(state => state.packsList.cardPacks)
+    // const handleChangePage = (event: unknown, newPage: number) => {
+    //     setPage(newPage);
+    // };
+    // const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //     setRowsPerPage(parseInt(event.target.value, 10));
+    //     setPage(0);
+    // };
+    // const emptyRows =
+    //     page > 0 ? Math.max(0, (1 + page) * rowsPerPage - selector.length) : 0;
     return (
         <div>
-    <Pagination count={10} shape="rounded"
-    />
-    <TablePagination
-        rowsPerPageOptions={[5, 10, 25]}
-        component="div"
-        count={selector.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-    />
+    {/*<Pagination count={10} shape="rounded"*/}
+    {/*/>*/}
+    {/*<TablePagination*/}
+    {/*    rowsPerPageOptions={[5, 10, 25]}*/}
+    {/*    component="div"*/}
+    {/*    count={selector.length}*/}
+    {/*    rowsPerPage={rowsPerPage}*/}
+    {/*    page={page}*/}
+    {/*    onPageChange={handleChangePage}*/}
+    {/*    onRowsPerPageChange={handleChangeRowsPerPage}*/}
+    {/*/>*/}
     </div>
     )
 }
