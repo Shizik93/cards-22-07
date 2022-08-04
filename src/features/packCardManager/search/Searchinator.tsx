5d220ea1fc6211fbab20ba18f3788a6159a5d325
YauhenKoltashau <yauhen.koltashau.1985@gmail.com>
2022-08-04 17:28:49 +0300

    commit 6  yauhen          added all

---
+++ src/features/packCardManager/search/Searchinator.tsx
@@ -0,0 +1,116 @@
+import React, {ChangeEvent, useEffect, useState} from 'react'
+import styles from './Search.module.css'
+import {useFormik} from "formik";
+import {useAppDispatch} from "../../../app/hooks";
+import {getCardsTC} from "../page/page-reducer";
+import {Button, FormControl, FormGroup, Grid, TextField} from "@material-ui/core";
+import SearchIcon from '@mui/icons-material/Search';
+
+import {useDebounce} from 'usehooks-ts'
+import {Divider, IconButton, InputBase, Paper} from "@mui/material";
+
+type SearchPropsType = {
+    // totalcardPacksCount: number
+    // currentPageNumber: number
+    // onClickPageChosen: (pageNumber: number) => void
+    // portionSize?: number
+    // minCardsCount: number
+    // maxCardsCount: number
+}
+
+export const Searchinator: React.FC<SearchPropsType> = () => {
+    const dispatch = useAppDispatch()
+    const formik = useFormik({
+        // validate: (values)=>{
+        // if(!values.email){
+        //     return {
+        //         search: 'Search is required'
+        //     }
+        // },
+        initialValues: {
+            packName: '',
+
+        },
+        onSubmit: values => {
+            dispatch(getCardsTC(values));
+        },
+    });
+    return (
+        <div className={styles.searchBlock}>
+            <Grid container justifyContent={'center'}>
+                <Grid item justifyContent={'center'}>
+                    <form onSubmit={formik.handleSubmit}>
+                        <FormControl>
+                            <FormGroup>
+                                <TextField
+                                    label="packName"
+                                    margin="normal"
+                                    {...formik.getFieldProps('packName')}
+                                />
+                                {formik.errors.packName ? <div>{formik.errors.packName}</div> : null}
+
+                                <Button type={'submit'} variant={'contained'} color={'primary'}>
+                                    Search
+                                </Button>
+                            </FormGroup>
+                        </FormControl>
+                    </form>
+                </Grid>
+            </Grid>
+
+        </div>
+    )
+}
+
+export default function Searchinator2() {
+    const dispatch = useAppDispatch()
+    const [value, setValue] = useState<string>('')
+    const debouncedValue = useDebounce<string>(value, 500)
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value)
+    }
+
+    // Fetch API (optional)
+    useEffect(() => {
+        dispatch(getCardsTC({packName: value }))
+        // Do fetch here...
+        // Triggers when "debouncedValue" changes
+    }, [debouncedValue])
+
+    return (
+        <div className={styles.searchBlock}>
+            <Paper
+                component="form"
+                sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+            >
+                <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+                    <SearchIcon />
+                </IconButton>
+                <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+                <InputBase
+                    sx={{ ml: 1, flex: 1 }}
+                    placeholder="Provide your text"
+                    inputProps={{ 'aria-label': 'Provide your text' }}
+                    value={value}
+                    onChange={handleChange}
+                />
+
+            </Paper>
+            {/*<Grid container justifyContent={'center'}>*/}
+            {/*    <Grid item justifyContent={'center'}>*/}
+            {/*        <Input type="text" value={value} onChange={handleChange}/>*/}
+            {/*    </Grid>*/}
+            {/*</Grid>*/}
+
+        </div>
+
+    )
+}
+
+
+
+
+
+
+
